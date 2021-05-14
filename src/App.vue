9ff6c3be447856bf9f5dfbfe7c569be4a7c08713
HenryBlairG <henry.blair.g@gmail.com>
2021-05-13 21:27:46 -0400

    feat(src>app): UPDATE app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
@@ -21,7 +21,25 @@ export default defineComponent({
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: var(--lght_clr);
+  border-radius: 5vh 5vh;
+  border-style: solid;
+  border-color: var(--dark_clr);
+  border-width: .15em;
+  box-shadow: 
+    .1em    .1em .1em .1em var(--dark_clr),
+    -.1em   .1em .1em .1em var(--dark_clr),
+    .1em   -.1em .1em .1em var(--dark_clr),
+    -.1em  -.1em .1em .1em var(--dark_clr),
+    .1em    .1em .2em .2em var(--dark_clr),
+    -.1em   .1em .2em .2em var(--dark_clr),
+    .1em   -.1em .2em .2em var(--dark_clr),
+    -.1em  -.1em .2em .2em var(--dark_clr),
+  ;
+  margin: 1em;
+  min-width: 800px;
+  min-height: 500px;
+  display: grid;
+  grid-template-columns: 80px 1fr;
 }
 </style>
