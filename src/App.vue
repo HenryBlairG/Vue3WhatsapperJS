088d4f883e5aa3652af1047ff9c56b996cc9e6e7
HenryBlairG <henry.blair.g@gmail.com>
2021-05-13 21:38:16 -0400

    refactor(src>app): FORMAT App.vue

--- src/App.vue
+++ src/App.vue
@@ -25,17 +25,15 @@ export default defineComponent({
   border-radius: 5vh 5vh;
   border-style: solid;
   border-color: var(--dark_clr);
-  border-width: .15em;
-  box-shadow: 
-    .1em    .1em .1em .1em var(--dark_clr),
-    -.1em   .1em .1em .1em var(--dark_clr),
-    .1em   -.1em .1em .1em var(--dark_clr),
-    -.1em  -.1em .1em .1em var(--dark_clr),
-    .1em    .1em .2em .2em var(--dark_clr),
-    -.1em   .1em .2em .2em var(--dark_clr),
-    .1em   -.1em .2em .2em var(--dark_clr),
-    -.1em  -.1em .2em .2em var(--dark_clr),
-  ;
+  border-width: 0.15em;
+  box-shadow: 0.1em 0.1em 0.1em 0.1em var(--dark_clr),
+    -0.1em 0.1em 0.1em 0.1em var(--dark_clr),
+    0.1em -0.1em 0.1em 0.1em var(--dark_clr),
+    -0.1em -0.1em 0.1em 0.1em var(--dark_clr),
+    0.1em 0.1em 0.2em 0.2em var(--dark_clr),
+    -0.1em 0.1em 0.2em 0.2em var(--dark_clr),
+    0.1em -0.1em 0.2em 0.2em var(--dark_clr),
+    -0.1em -0.1em 0.2em 0.2em var(--dark_clr);
   margin: 1em;
   min-width: 800px;
   min-height: 500px;
