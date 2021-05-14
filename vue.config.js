7371fb1b56a27bac6c8b449d17ca7180adb069e6
HenryBlairG <henry.blair.g@gmail.com>
2021-05-13 21:26:22 -0400

    feat(assets>styles): ADD stylesheets

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
-module.exports = process.env.ENVIRONMENT === 'development' ? {} : {
+module.exports = process.env.ENVIRONMENT === 'development' ? {
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/assets/styles/main.scss";`,
+      },
+    },
+  },
+} : {
   publicPath: "/Vue3WhatsapperJS/",
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/assets/styles/main.scss";`,
+      },
+    },
+  },
 };
